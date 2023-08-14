e8afbc1ca0acdcca7b2a31b02b6c77cf843204ac
devDC <info@datacentral.pro>
2023-08-13 21:51:35 -0800

    Modified App.js for testing

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Seamless Data Solutions Await at Data Central Pro!
+        </p>
+        <a
+          className="App-link"
+          href="#"
+          // </header>target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
